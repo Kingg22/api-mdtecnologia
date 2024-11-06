c1b3bf54e2f0d0f8decf03d567eb130bb208c121
Kingg <rey2213@outlook.com>
2024-11-05 22:31:14 -0500

    feat: add nlog configuration

---
+++ MD Tech/LogsApi.cs
@@ -0,0 +1,44 @@
+﻿using NLog;
+
+namespace MD_Tech
+{
+    internal class LogsApi
+    {
+        private readonly Logger LOGGER;
+
+        public LogsApi()
+        {
+            LOGGER = LogManager.GetLogger("Anonimous Class");
+        }
+
+        public LogsApi(Type classType)
+        {
+            LOGGER = LogManager.GetLogger(classType.FullName);
+        }
+
+        public void Informacion(string mensaje)
+        {
+            this.LOGGER.Info(mensaje);
+        }
+
+        public void Advertencia(string mensaje)
+        {
+            this.LOGGER.Warn(mensaje);
+        }
+
+        public void Errores(string mensaje)
+        {
+            this.LOGGER.Error(mensaje);
+        }
+
+        public void Depuracion(string mensaje)
+        {
+            this.LOGGER.Debug(mensaje);
+        }
+
+        public void Excepciones(Exception exception, string mensaje)
+        {
+            this.LOGGER.Error(exception, mensaje);
+        }
+    }
+}
