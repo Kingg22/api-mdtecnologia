a358ea8a57dcf6d4fbb4c65a3067b7de4a388ce4
LuisGpt <luis.monterrosa1@utp.ac.pa>
2024-11-07 19:37:58 -0500

    feat(Clientes): add controller
    TODO add cliente and usuario

---
+++ MD Tech/DTOs/ClienteDto.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MD_Tech.DTOs
+{
+    public class ClienteDto
+    {
+        public Guid? Id { get; set; }
+        
+        [Required]
+        public required string Nombre { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 1)]
+        public required string Apellido { get; set; }
+
+        public required string Correo { get; set; }
+
+        public string? Telefono { get; set; }
+        [Required]
+        public Guid? IdUsuario { get; set; }
+
+    }
+}
