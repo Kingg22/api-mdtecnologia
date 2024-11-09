5441003f0b5441dc94b62888b75b88d65c5f457a
Kingg <rey2213@outlook.com>
2024-11-09 15:18:19 -0500

    feat: add producto controller

---
+++ MD Tech/DTOs/ProductoProveedorDto.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MD_Tech.DTOs
+{
+    public class ProductoProveedorDto
+    {
+        public Guid? Producto { get; set; }
+        
+        [Required]
+        public Guid Proveedor { get; set; }
+
+        [Required]
+        [Range(0.00, double.MaxValue)]
+        public decimal Precio { get; set; }
+
+        [Required]
+        [Range(0.00, double.MaxValue)]
+        public decimal Impuesto { get; set; } = decimal.Zero;
+
+        public decimal? Total { get; set; }
+
+        public int? Stock { get; set; }
+
+        public DateOnly? FechaActualizado { get; set; }
+    }
+}
---
+++ MD Tech/DTOs/ProductosDto.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MD_Tech.DTOs
+{
+    public class ProductosDto
+    {
+        public Guid? Id { get; set; }
+
+        [Required]
+        public required string Nombre { get; set; }
+
+        [Required]
+        public required string Marca { get; set; }
+
+        public string? Descripcion { get; set; }
+
+        public Guid? Categoria { get; set; }
+
+        // Representa una imagen guardada como archivo y con link interno de la api para obtenerla
+        public string? Imagen1 { get; set; }
+
+        // Representa la imagen de internet como link
+        public string? Imagen2 { get; set; }
+
+        public ICollection<ProductoProveedorDto>? Proveedores { get; set; } = [];
+    }
+}
