63fa86e00f63bc1bff9e63cb19818a3ce6084fca
Kingg22 <89825425+Kingg22@users.noreply.github.com>
2024-11-11 10:31:05 -0500

    feat(proveedor): add controller and dto
    reviewed by Kingg, made by LuisGPT

---
+++ MD Tech/DTOs/DireccionDto.cs
@@ -0,0 +1,15 @@
+﻿using NodaTime;
+
+namespace MD_Tech.DTOs
+{
+    public class DireccionDto
+    {
+        public Guid? Id { get; set; }
+
+        public string? Descripcion { get; set; }
+
+        public int? Provincia { get; set; }
+
+        public LocalDateTime? CreatedAt { get; set; }
+    }
+}
