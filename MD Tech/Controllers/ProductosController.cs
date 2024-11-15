cd950d100bfcefa0a29b0e83830e7211a694e193
Kingg22 <89825425+Kingg22@users.noreply.github.com>
2024-11-15 18:50:34 -0500

    style(controllers): clean code

--- MD Tech/Controllers/ProductosController.cs
+++ MD Tech/Controllers/ProductosController.cs
@@ -17,14 +17,14 @@ namespace MD_Tech.Controllers
     public class ProductosController : ControllerBase
     {
         private readonly MdtecnologiaContext MdTecnologiaContext;
-        private readonly LogsApi logs;
+        private readonly LogsApi<ProductosController> logger;
         private readonly IStorageApi storageApi;
 
-        public ProductosController(MdtecnologiaContext MdTecnologiaContext, IStorageApi storageApi)
+        public ProductosController(MdtecnologiaContext MdTecnologiaContext, IStorageApi storageApi, LogsApi<ProductosController> logger)
         {
             this.MdTecnologiaContext = MdTecnologiaContext;
             this.storageApi = storageApi;
-            logs = new LogsApi(GetType());
+            this.logger = logger;
         }
 
         [HttpGet]
@@ -33,22 +33,14 @@ namespace MD_Tech.Controllers
         public async Task<ActionResult<List<ProductosDto>>> GetProductos([FromQuery] ProductosRequestDto paginacionDto)
         {
             if (paginacionDto.Page < 0 || paginacionDto.Limit < 0)
-            {
                 return BadRequest(new { paginacion = "las números de paginación o límites no pueden ser negativos" });
-            }
             var query = MdTecnologiaContext.Productos.AsQueryable();
             if (!string.IsNullOrWhiteSpace(paginacionDto.Nombre))
-            {
                 query = query.Where(p => EF.Functions.ILike(p.Nombre, $"%{paginacionDto.Nombre}%"));
-            }
             if (!string.IsNullOrWhiteSpace(paginacionDto.Marca))
-            {
                 query = query.Where(p => EF.Functions.ILike(p.Marca, $"%{paginacionDto.Marca}%"));
-            }
             if (paginacionDto.Categoria != null)
-            {
                 query = query.Where(p => p.Categoria == paginacionDto.Categoria);
-            }
             if (!string.IsNullOrWhiteSpace(paginacionDto.OrderBy))
             {
                 var orderBy = paginacionDto.OrderBy.Split("-");
@@ -62,22 +54,17 @@ namespace MD_Tech.Controllers
                         : query.OrderBy(e => EF.Property<object>(e, property.Name));
                 }
                 else
-                {
                     return BadRequest(new { OrderBy = "no se ha podido determinar la columna para ordernar sus resultados, formato esperado es 'columna-desc'" });
-                }
             }
             else
-            {
                 query = query.OrderBy(p => p.Id);
-            }
+
             var totalProducts = await MdTecnologiaContext.Productos.CountAsync();
             var hasNextPage = (paginacionDto.Page + 1) * paginacionDto.Limit < totalProducts;
             // Calcula la página anterior que contiene resultados
             int previousPage = paginacionDto.Page - 1;
             while (previousPage > 0 && previousPage * paginacionDto.Limit >= totalProducts)
-            {
                 previousPage--;
-            }
 
             var nextUrl = hasNextPage
                 ? Url.Action(
@@ -152,28 +139,21 @@ namespace MD_Tech.Controllers
         {
             if (id != productoDto.Id)
             {
-                logs.Informacion($"Producto de id {id} no coincide con el body id {productoDto.Id}");
-                return BadRequest();
+                logger.Informacion($"Producto de id {id} no coincide con el body id {productoDto.Id}");
+                return BadRequest(new { id = "El id del body no coincide con la ruta" });
             }
             var producto = await MdTecnologiaContext.Productos.FindAsync(id);
             if (producto == null)
-            {
                 return NotFound(new { id = "producto no encontrado" });
-            }
             // Campos obligatorios tipo string
             if (string.IsNullOrWhiteSpace(productoDto.Nombre) || string.IsNullOrWhiteSpace(productoDto.Marca))
-            {
                 return BadRequest(new { fieldsString = "campos como Nombre o Marca son requeridos" });
-            }
             // Descripción no es null se valida tipo string
             if (productoDto.Descripcion != null && string.IsNullOrWhiteSpace(productoDto.Descripcion))
-            {
                 return BadRequest(new { descripcion = "la descripción no puede ser espacios en blanco" });
-            }
             if (productoDto.Categoria != null && await MdTecnologiaContext.Categorias.FindAsync(productoDto.Categoria) == null)
-            {
                 return NotFound(new { categoria = "categoria de producto no encontrada" });
-            }
+
             using var transaction = await MdTecnologiaContext.Database.BeginTransactionAsync();
             try
             {
@@ -192,7 +172,7 @@ namespace MD_Tech.Controllers
                             var proveedor = await MdTecnologiaContext.Proveedores.FindAsync(proveedorDto.Proveedor);
                             if (proveedor == null)
                             {
-                                logs.Advertencia($"Se ha omitido la relación producto - proveedor con Id {proveedorDto.Proveedor} no encontrado, precio = {proveedorDto.Precio} + impuesto = {proveedorDto.Impuesto}");
+                                logger.Advertencia($"Se ha omitido la relación producto - proveedor con Id {proveedorDto.Proveedor} no encontrado, precio = {proveedorDto.Precio} + impuesto = {proveedorDto.Impuesto}");
                                 continue;
                             }
                             var relacion = await MdTecnologiaContext.ProductosProveedores.FirstOrDefaultAsync(pp => pp.Producto == id && pp.Proveedor == proveedorDto.Proveedor);
@@ -225,7 +205,7 @@ namespace MD_Tech.Controllers
                     }
                     catch (Exception ex)
                     {
-                        logs.Excepciones(ex, "Error al actualizar relacionaciones producto - proveedor");
+                        logger.Excepciones(ex, "Error al actualizar relacionaciones producto - proveedor");
                         await transaction.RollbackToSavepointAsync("producto");
                     }
                 }
@@ -235,14 +215,14 @@ namespace MD_Tech.Controllers
                 {
                     try
                     {
-                        logs.Informacion("Relaciones de imagenes producto encontradas. Procesando...");
+                        logger.Informacion("Relaciones de imagenes producto encontradas. Procesando...");
                         foreach (var imagenDto in productoDto.Imagenes)
                         {
                             if (!string.IsNullOrWhiteSpace(imagenDto.Url) && IsValidUrl(imagenDto.Url))
                             {
                                 if (imagenDto.Descripcion != null && string.IsNullOrEmpty(imagenDto.Descripcion))
                                 {
-                                    logs.Advertencia($"Colocando null a descripción inválida, url: {imagenDto.Url}");
+                                    logger.Advertencia($"Colocando null a descripción inválida, url: {imagenDto.Url}");
                                     imagenDto.Descripcion = null;
                                 }
                                 var imagenProducto = new ImagenesProducto()
@@ -255,7 +235,7 @@ namespace MD_Tech.Controllers
                             }
                             else
                             {
-                                logs.Advertencia($"Omitiendo una url no válida la imagen: {imagenDto.Url}");
+                                logger.Advertencia($"Omitiendo una url no válida la imagen: {imagenDto.Url}");
                                 continue;
                             }
                         }
@@ -263,19 +243,19 @@ namespace MD_Tech.Controllers
                     }
                     catch (Exception excep)
                     {
-                        logs.Excepciones(excep, "ha ocurrido un error al guardar las imagenes del producto");
+                        logger.Excepciones(excep, "ha ocurrido un error al guardar las imagenes del producto");
                         await transaction.RollbackToSavepointAsync("proveedores");
                     }
                 }
 
                 await transaction.CommitAsync();
-
+                logger.Informacion($"Se ha actualizado el producto {producto.Id}");
                 await MdTecnologiaContext.Entry(producto).ReloadAsync();
                 return Ok(new { producto = new ProductosDto(producto) });
             }
             catch (Exception ex)
             {
-                logs.Excepciones(ex, "Error general al actualiza producto");
+                logger.Excepciones(ex, "Error general al actualiza producto");
                 await transaction.RollbackAsync();
                 return Problem();
             }
@@ -290,25 +270,18 @@ namespace MD_Tech.Controllers
         [SwaggerResponse(500, "Ha ocurrido un error inesperado")]
         public async Task<ActionResult<ProductosDto>> PostProductos([FromBody] ProductosDto productoDto)
         {
-            logs.Informacion($"Iniciando la creación del producto con ID {productoDto.Id} y nombre {productoDto.Nombre}");
+            logger.Informacion($"Iniciando la creación del producto con ID {productoDto.Id} y nombre {productoDto.Nombre}");
             if (productoDto.Id != null && await MdTecnologiaContext.Productos.FindAsync(productoDto.Id) != null)
-            {
                 return BadRequest(new { Id = "id proporcionado en uso" });
-            }
             // Campos obligatorios tipo string
             if (string.IsNullOrWhiteSpace(productoDto.Nombre) || string.IsNullOrWhiteSpace(productoDto.Marca))
-            {
                 return BadRequest(new { fieldsString = "campos como Nombre o Marca son requeridos" });
-            }
             // Descripción no es null se valida tipo string
             if (productoDto.Descripcion != null && string.IsNullOrWhiteSpace(productoDto.Descripcion))
-            {
                 return BadRequest(new { descripcion = "la descripción no puede ser espacios en blanco" });
-            }
             if (productoDto.Categoria != null && await MdTecnologiaContext.Categorias.FindAsync(productoDto.Categoria) == null)
-            {
                 return NotFound(new { categoria = "categoria de producto no encontrada" });
-            }
+
             using var transaction = await MdTecnologiaContext.Database.BeginTransactionAsync();
             try
             {
@@ -329,12 +302,12 @@ namespace MD_Tech.Controllers
                 {
                     try
                     {
-                        logs.Informacion("Relaciones de proveedores encontradas, procesando...");
+                        logger.Informacion("Relaciones de proveedores encontradas, procesando...");
                         foreach (var proveedorDto in productoDto.Proveedores)
                         {
                             if (await MdTecnologiaContext.Proveedores.FindAsync(proveedorDto.Proveedor) == null)
                             {
-                                logs.Advertencia($"Se ha omitido la relación producto - proveedor con Id {proveedorDto.Proveedor} no encontrado, precio = {proveedorDto.Precio} + impuesto = {proveedorDto.Impuesto}");
+                                logger.Advertencia($"Se ha omitido la relación producto - proveedor con Id {proveedorDto.Proveedor} no encontrado, precio = {proveedorDto.Precio} + impuesto = {proveedorDto.Impuesto}");
                                 continue;
                             }
                             var productoProveedor = new ProductosProveedor()
@@ -353,7 +326,7 @@ namespace MD_Tech.Controllers
                     }
                     catch (Exception ex)
                     {
-                        logs.Excepciones(ex, "Excepción al crear producto relacionado a proveedor, revirtiendo a savepoint");
+                        logger.Excepciones(ex, "Excepción al crear producto relacionado a proveedor, revirtiendo a savepoint");
                         await transaction.RollbackToSavepointAsync("producto");
                     }
                 }
@@ -364,14 +337,14 @@ namespace MD_Tech.Controllers
                 {
                     try
                     {
-                        logs.Informacion("Relaciones de imagenes producto encontradas. Procesando...");
+                        logger.Informacion("Relaciones de imagenes producto encontradas. Procesando...");
                         foreach (var imagenDto in productoDto.Imagenes)
                         {
                             if (!string.IsNullOrWhiteSpace(imagenDto.Url) && IsValidUrl(imagenDto.Url))
                             {
                                 if (imagenDto.Descripcion != null && string.IsNullOrEmpty(imagenDto.Descripcion))
                                 {
-                                    logs.Advertencia($"Colocando null a descripción inválida, url: {imagenDto.Url}");
+                                    logger.Advertencia($"Colocando null a descripción inválida, url: {imagenDto.Url}");
                                     imagenDto.Descripcion = null;
                                 }
                                 var imagenProducto = new ImagenesProducto()
@@ -384,7 +357,7 @@ namespace MD_Tech.Controllers
                             }
                             else
                             {
-                                logs.Advertencia($"Omitiendo una url no válida la imagen: {imagenDto.Url}");
+                                logger.Advertencia($"Omitiendo una url no válida la imagen: {imagenDto.Url}");
                                 continue;
                             }
                         }
@@ -392,18 +365,19 @@ namespace MD_Tech.Controllers
                     }
                     catch (Exception excep)
                     {
-                        logs.Excepciones(excep, "ha ocurrido un error al guardar las imagenes del producto");
+                        logger.Excepciones(excep, "ha ocurrido un error al guardar las imagenes del producto");
                         await transaction.RollbackToSavepointAsync("proveedores");
                     }
                 }
 
                 await transaction.CommitAsync();
                 await MdTecnologiaContext.Entry(productos).ReloadAsync();
+                logger.Informacion($"Se ha creado un nuevo producto Id: {productos.Id} Nombre: {productos.Nombre}");
                 return Created(Url.Action(nameof(GetProductos), "Productos", new { id = productos.Id }, Request.Scheme), new { producto = new ProductosDto(productos) });
             }
             catch (Exception ex)
             {
-                logs.Excepciones(ex, "Excepción general al crear producto");
+                logger.Excepciones(ex, "Excepción general al crear producto");
                 await transaction.RollbackAsync();
                 return Problem("Ha ocurrido un error al guardar en la base de datos, revisar si el producto solo fue creado");
             }
@@ -420,19 +394,13 @@ namespace MD_Tech.Controllers
         public async Task<ActionResult> UploadImage(Guid id, IFormFile image)
         {
             if (image == null || image.Length == 0)
-            {
                 return BadRequest(new { image = "No se ha proporcionado un archivo de imagen válido." });
-            }
             if (!image.ContentType.StartsWith("image/"))
-            {
                 return BadRequest(new { image = "El archivo debe ser una imagen válida." });
-            }
             // 5 MB en bytes limitado por tema de rendimiento del fronted y costo de almacenamiento
             const long maxSizeInBytes = 5 * 1024 * 1024;
             if (image.Length > maxSizeInBytes)
-            {
                 return BadRequest(new { image = "El archivo de imagen excede el tamaño máximo permitido de 5 MB." });
-            }
 
             var producto = await MdTecnologiaContext.Productos.FindAsync(id);
             if (producto != null)
@@ -455,14 +423,10 @@ namespace MD_Tech.Controllers
                     return Accepted(result.Url);
                 }
                 else
-                {
                     return Problem();
-                }
             }
             else
-            {
                 return NotFound();
-            }
         }
 
         [HttpDelete("{id}")]
@@ -474,10 +438,8 @@ namespace MD_Tech.Controllers
         {
             var productos = await MdTecnologiaContext.Productos.FindAsync(id);
             if (productos == null)
-            {
                 return NotFound();
-            }
-            logs.Advertencia($"Se va a eliminar el producto con Id: {id} y nombre: {productos.Nombre}");
+            logger.Advertencia($"Se va a eliminar el producto con Id: {id} y nombre: {productos.Nombre}");
             MdTecnologiaContext.Productos.Remove(productos);
             await MdTecnologiaContext.SaveChangesAsync();
 
@@ -494,25 +456,20 @@ namespace MD_Tech.Controllers
         {
             var producto = await MdTecnologiaContext.Productos.Include(p => p.ImagenesProductos).FirstOrDefaultAsync(p => p.Id == id);
             if (producto == null)
-            {
                 return NotFound(new { producto = "No fue encontrado el producto" });
-            }
             var imagen = producto.ImagenesProductos.FirstOrDefault(imagenes => imagenes.Id == idImagen);
             if (imagen == null)
-            {
                 return NotFound(new { imagen = "no fue encontrada la imagen con ese Id para ese producto" });
-            }
             if (string.IsNullOrWhiteSpace(imagen.Descripcion))
-            {
                 return Problem("No se pudo obtener el nombre para eliminar su referencia en el servidor.");
-            }
+            
             var response = await storageApi.DeleteObjectAsync(imagen.Descripcion);
             if (response)
             {
                 MdTecnologiaContext.Remove(imagen);
                 await MdTecnologiaContext.SaveChangesAsync();
                 await MdTecnologiaContext.Entry(producto).ReloadAsync();
-                logs.Advertencia($"Se ha eliminado una imagen del producto {producto.Id}");
+                logger.Advertencia($"Se ha eliminado una imagen del producto {producto.Id}");
                 return Ok(new ProductosDto(producto));
             }
             return Problem("No se pudo eliminar la imagen en el servicio utilizado");
