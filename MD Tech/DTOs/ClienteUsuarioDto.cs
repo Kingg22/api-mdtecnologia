917b1b6ea245e28cefc31b0cf1c161ec6443b933
Kingg22 <89825425+Kingg22@users.noreply.github.com>
2024-11-20 14:53:11 -0500

    feat(clientes): add better validations for email
    add insert direcciones to /Clientes/usuario

--- MD Tech/DTOs/ClienteUsuarioDto.cs
+++ MD Tech/DTOs/ClienteUsuarioDto.cs
@@ -4,32 +4,33 @@ namespace MD_Tech.DTOs
 {
     public class ClienteUsuarioDto
     {
-        public Guid? UsuarioID { get; set; }
+        public Guid? UsuarioId { get; set; }
 
-        public Guid? ClienteID { get; set; }
-        
-        [Required]
-        [StringLength(70, MinimumLength = 8)]
-        public required string Password { get; set; }
-
-        public bool Disabled { get; set; } = false;
-
-        [Required]
-        public required string Username { get; set; }
+        public Guid? ClienteId { get; set; }
 
         [Required]
         [StringLength(100, MinimumLength = 1)]
-        public required string Nombre { get; set; }
+        public string Nombre { get; set; } = null!;
 
         [Required]
         [StringLength(100, MinimumLength = 1)]
-        public required string Apellido { get; set; }
+        public string Apellido { get; set; } = null!;
 
         [Required]
         [StringLength(255, MinimumLength = 1)]
-        public required string Correo { get; set; }
+        public string Correo { get; set; } = null!;
 
         public string? Telefono { get; set; }
 
+        public ICollection<DireccionDto> Direcciones { get; set; } = [];
+
+        [Required]
+        public string Username { get; set; } = null!;
+
+        [Required]
+        [StringLength(70, MinimumLength = 8)]
+        public string Password { get; set; } = null!;
+
+        public bool Disabled { get; set; } = false;
     }
 }
